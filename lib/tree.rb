2be533007f399ffc858b2f9b6045ae0d3a19e1ca
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-05-11 09:14:06 +0100

    commit tree 0b22ff623d4e455ddf2b10ff6dba5fe81aa8bbb5

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,4 +2,16 @@ class Tree
   def initialize(root = nil)
     @root = root
   end
+
+  def get_element_by_id(id)
+    nodes_to_visit = [@root]
+
+    while nodes_to_visit.length > 0
+      node = nodes_to_visit.shift
+      return node if node[:id] == id
+      nodes_to_visit = node[:children] + nodes_to_visit
+    end
+
+    nil
+  end
 end
